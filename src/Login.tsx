e6be41dfcc085fa91bf4a7f7d510b5bc21023441
unknown <fuaad29@gmail.com>
2024-07-02 11:47:22 +0500

    added profle page

--- src/Login.tsx
+++ src/Login.tsx
@@ -20,75 +20,76 @@ const Login: React.FC = () => {
 
         notification.success({
           message: 'Login Successful',
-          description: `Welcome back,!`,
+          description: 'Welcome back!',
         });
-        const token = response.data.token
-        localStorage.setItem("token", token)
+        const token = response.data.token;
+        localStorage.setItem("token", token);
         console.log('localStorage:', localStorage);
 
         navigate("/profile");
       })
       .catch(error => {
         console.error('error', error);
-  
+
         notification.error({
           message: 'Login Failed',
           description: 'Please check your credentials and try again.',
         });
       });
   };
-  
+
   const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
 
   return (
-  <div className='bg-gray-100 p-12 rounded-xl'>
-    <h2 className='font-bold text-2xl mb-16'>Login</h2>
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item<FieldType>
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: 'Please input your email!' }]}
+    <div className='bg-gray-100 p-12 rounded-xl'>
+      <h2 className='font-bold text-2xl mb-16'>Login</h2>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input />
-      </Form.Item>
+        <Form.Item<FieldType>
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: 'Please input your email!' }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item<FieldType>
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item<FieldType>
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{ offset: 8, span: 16 }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+        <Form.Item<FieldType>
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-    <span className="text-sm text-black">Not registered yet? <a className='cursor-pointer' onClick={()=>navigate("/register")}>
-      Register Now</a>
-    </span>
-  </div>
-)};
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <span className="text-sm text-black">Not registered yet? <a className='cursor-pointer' onClick={()=>navigate("/register")}>
+        Register Now</a>
+      </span>
+    </div>
+  );
+};
 
 export default Login;
