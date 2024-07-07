51f3ad40f2b662d11757edb5e103884c477fa092
unknown <fuaad29@gmail.com>
2024-07-07 12:37:33 +0500

    post section deon

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router";
 
 export default function Profile() {
   const [userPosts, setUserPosts] =useState([])
+  const [allPosts, setAllPosts] =useState([])
+
   const navigate = useNavigate()
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -18,6 +20,19 @@ export default function Profile() {
     .then((response) => {
       console.log("response:", response);
       setUserPosts(response.data)
+    })
+    .catch((error) => {
+      console.log("error:", error);
+    });
+
+    axios.get("http://127.0.0.1:3000/all_posts", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then((response) => {
+      console.log("response:", response);
+      setAllPosts(response.data)
     })
     .catch((error) => {
       console.log("error:", error);
@@ -39,22 +54,48 @@ export default function Profile() {
       notification.error({message: ""})
     })
   }
+
+  const logOut = () => {
+    localStorage.clear()
+    notification.success({message: "Logged Out Successfully"})
+    navigate("/login")
+  }
   return (
     <>
-      <p className="text-lg font-bold">Your Posts: {userPosts.length}</p>
-      {userPosts.map((post, index)=>{
-        const {_id} = post
-        return (
-          <div className="flex gap-12" key={index}>
-            <p>
-              {index}{post.content}
-              <span className="text-blue-600 cursor-pointer" onClick={()=>{editComment(post)}}>Edit</span>
-              <span className="text-red-600 cursor-pointer" onClick={()=>{deleteComment(_id)}}>Delete</span>
-            </p>
-          </div>
-        )
-      })}
-      <Button type="primary" onClick={()=>{navigate("/new_post")}}>Add New</Button>
+    <button onClick={logOut}>Log Out</button>
+      <div>
+        <p className="text-lg font-bold">Your Posts: {userPosts.length}</p>
+        {userPosts.map((post, index)=>{
+          const {_id} = post
+          return (
+            <div className="flex gap-12" key={index}>
+              <p>
+                {post.content}<span className="text-gray-400">{post.user_name}</span>
+                <span className="text-blue-600 cursor-pointer" onClick={()=>{editComment(post)}}>Edit</span>
+                <span className="text-red-600 cursor-pointer" onClick={()=>{deleteComment(_id)}}>Delete</span>
+              </p>
+            </div>
+          )
+        
+        })}
+        <Button type="primary" onClick={()=>{navigate("/new_post")}}>Add New</Button>
+      </div>
+
+      <div>
+        <p className="text-lg font-bold">All Posts: {userPosts.length}</p>
+        {allPosts.map((post, index)=>{
+          const {_id} = post
+          return (
+            <div className="flex gap-12" key={index}>
+              <p>
+                {post.content}<span className="text-gray-400">{post.user_name}</span>
+                <span className="text-blue-600 cursor-pointer" onClick={()=>{editComment(post)}}>Edit</span>
+                <span className="text-red-600 cursor-pointer" onClick={()=>{deleteComment(_id)}}>Delete</span>
+              </p>
+            </div>
+          )
+        })}
+      </div>
     </>
   );
 }
