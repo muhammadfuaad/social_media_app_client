c77ad066889c2d0384bb386058a9f2769713a10a
unknown <fuaad29@gmail.com>
2024-07-15 22:25:21 +0500

    modified input

--- src/Post.jsx
+++ src/Post.jsx
@@ -10,6 +10,7 @@ export default function Post({post, loggedUserId, index}) {
   const [comment, setComment] =useState("")
   const [comments, setComments] =useState([])
   const [showComments, setShowComments] =useState(false)
+  const [showSubmit, setShowSubmit] = useState(false)
 
   const token = localStorage.getItem("token")
 
@@ -41,6 +42,7 @@ export default function Post({post, loggedUserId, index}) {
   // comment actions 
   const addComment =(postId) => {
     console.log(postId);
+    console.log(comment);
     axios.post(`http://127.0.0.1:3000/add_comment/${postId}`, {content: comment}, {
       headers: {
         Authorization: `Bearer ${token}`
@@ -84,6 +86,13 @@ export default function Post({post, loggedUserId, index}) {
     console.log(comments);
   }, [comments])
 
+  const handleInput = (e) => {
+    if (e.target.value !== 0) {
+      setShowSubmit(true)
+    }
+    setComment(e.target.value)
+  }
+
   const items = [
     {
       key: '1',
@@ -122,18 +131,19 @@ export default function Post({post, loggedUserId, index}) {
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{post.content}</p>
           <span className="text-blue-500 text-sm cursor-pointer" onClick={()=>{setShowComments(!showComments)}}>{comments.length} comments</span>
           
-          <div className="flex gap-12">
-            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white
-              bg-blue-700 rounded-lg" onClick={()=>{setShowInput(!showInput)}}
-            >
-              Comment
-            </a>
+          <div className="flex gap-12 text-gray-600 cursor-pointer">
+            <span onClick={()=>{setShowInput(!showInput)}}>Comment</span>
+            
             <span onClick={()=>{}}>Like</span>
           </div>
           {showInput &&
-            (<div className="flex">
-              <input className="rounded border-black border block w-full" onChange={(e)=>{setComment(e.target.value)}}></input>
-              <button className="bg-blue-600 text-white" onClick={()=>{addComment(post._id)}}>Submit</button>
+            (<div className="flex gap-4 justify-center items-center">
+              <img className="w-6 h-6 mb-3 rounded-full shadow-lg" src="./avatar.svg" alt="Bonnie image"/>
+              <input className="border-gray-600 border block w-full rounded-3xl p-2" onChange={(e)=>{handleInput(e)}}></input>
+              {showSubmit && 
+              <button className="bg-blue-600 text-white rounded-3xl text-sm font-bold" onClick={()=>{addComment(post._id)}}>
+                Submit
+              </button>}
             </div>)
           }
         </div>
@@ -145,7 +155,7 @@ export default function Post({post, loggedUserId, index}) {
               <div className="flex gap-2">
                 <img className="w-6 h-6 mb-3 rounded-full shadow-lg" src="./avatar.svg" alt="Bonnie image"/>
                 <a href="#">
-                  <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{comment.loggedUserId.name}</h5>
+                  <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{comment.userId.name}</h5>
                 </a>
               </div>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{comment.content}</p>
